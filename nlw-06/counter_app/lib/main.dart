e9f06e73395647bf2548777653c22456588f0de5
Marcos <marcosvidal@outlook.com>
2021-06-21 21:22:41 -0300

    Adicionado fontes e cores

--- nlw-06/counter_app/lib/main.dart
+++ nlw-06/counter_app/lib/main.dart
@@ -1,11 +1,10 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(primarySwatch: Colors.purple),
-    home: BackGroundImage(),
+    home: HomePage(),
     
   ));
 }
@@ -32,40 +31,33 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Center(child: Text('Primeiro APP Contador')),
       ),
-      body: 
+      body:
+     
+      Container(
+       decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage("https://i.imgur.com/vigvPbF.png"),
+                fit: BoxFit.cover)),
+        padding: EdgeInsets.only(top: 50.0),
+        child: Center(
+          
+            child: Text(
+          'Contador\n$count' + '\n\n\n\nTioPew',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+          
+        )),
+      ),
       
-      Center(
-          child: Text(
-        'Contador\n$count' + '\nTioPew',
-        textAlign: TextAlign.center,
-      )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           increment();
         },
+       
       ),
-    );
-  }
-}
-
-class BackGroundImage extends StatelessWidget {
-  const BackGroundImage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage("https://i.imgur.com/vigvPbF.png"),
-                fit: BoxFit.cover)),
-        padding: EdgeInsets.only(top: 50.0),
-      ),
-      
-    );
     
+    );
   }
 }
+
