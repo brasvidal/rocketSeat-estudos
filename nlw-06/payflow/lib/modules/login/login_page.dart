2170f86bf9fb17077bbdca1d52ffeb768a67888b
Marcos <marcosvidal@outlook.com>
2021-06-23 06:43:24 -0300

    Adicionado login com Firebase e classe controller_auth

--- nlw-06/payflow/lib/modules/login/login_page.dart
+++ nlw-06/payflow/lib/modules/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -13,6 +15,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -59,10 +62,9 @@ class _LoginPageState extends State<LoginPage> {
                       padding:
                           const EdgeInsets.only(left: 40, right: 40, top: 40),
                       child: SocialLoginButton(
-                        onTap: () {
-                      print('click');
-                        },
-                      ),
+                        onTap: controller.googleSignIn,
+                        
+                      )
                     ),
                   ],
                 ))
