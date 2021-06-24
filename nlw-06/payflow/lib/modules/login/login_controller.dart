ebf0a1d0f94aa5ace258f1e28be65c95fe1fb378
Marcos <marcosvidal@outlook.com>
2021-06-24 20:39:13 -0300

    finalizado aula 4 na aula 5 inserido funcoes de digitar o boleto

--- nlw-06/payflow/lib/modules/login/login_controller.dart
+++ nlw-06/payflow/lib/modules/login/login_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/models/user_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController {
   final authController = AuthController();
