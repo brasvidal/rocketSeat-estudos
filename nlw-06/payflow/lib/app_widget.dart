692de940ec8f7f0d64cb02dee541962b1f2a1c17
Marcos <marcosvidal@outlook.com>
2021-06-24 17:49:30 -0300

    Finalizado Aula 4, adicionado camera leitura etc

--- nlw-06/payflow/lib/app_widget.dart
+++ nlw-06/payflow/lib/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/bar_code_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 
 import 'modules/splash/splash_page.dart';
@@ -29,6 +30,7 @@ class AppWidget extends StatelessWidget {
         '/login': (context) => LoginPage(),
         '/splash': (context) => SplashPage(),
         '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_boleto': (context) => InsertBoletoPage(),
       },
     );
   }
